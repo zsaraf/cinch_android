502c73411627d4e0c0ed7655b6c2af15b633013f
Lilli Oetting <loetting@stanford.edu>
2015-07-23 11:20:58 -0700

    settings page + seshdialog

---
+++ app/src/main/java/com/seshtutoring/seshapp/view/components/SeshDialog.java
@@ -0,0 +1,7 @@
+package com.seshtutoring.seshapp.view.components;
+
+/**
+ * Created by lillioetting on 7/22/15.
+ */
+public class SeshDialog {
+}
---
+++ app/src/main/java/com/seshtutoring/seshapp/view/fragments/CashoutDialogFragment.java
@@ -0,0 +1,7 @@
+package com.seshtutoring.seshapp.view.fragments;
+
+/**
+ * Created by lillioetting on 7/20/15.
+ */
+public class CashoutDialogFragment {
+}
