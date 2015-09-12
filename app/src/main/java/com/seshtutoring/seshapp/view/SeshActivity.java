9705046f0cf885e5aa1ed1f6e85ef154ed2bb8fb
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2015-09-11 19:10:18 -0700

    status bar coloring fixes

--- app/src/main/java/com/seshtutoring/seshapp/view/SeshActivity.java
+++ app/src/main/java/com/seshtutoring/seshapp/view/SeshActivity.java
@@ -10,6 +10,7 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
