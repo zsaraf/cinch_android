fe10a1795c69079ec2ac0a708b790c6496c683b9
Lilli Oetting <loetting@stanford.edu>
2015-09-14 18:10:25 -0700

    activity indicator and dialogs added to change password

--- app/src/main/java/com/seshtutoring/seshapp/view/ChangePasswordActivity.java
+++ app/src/main/java/com/seshtutoring/seshapp/view/ChangePasswordActivity.java
@@ -1,5 +1,7 @@
 package com.seshtutoring.seshapp.view;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.app.ActionBar;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -26,21 +28,30 @@ import com.seshtutoring.seshapp.model.Sesh;
 import com.seshtutoring.seshapp.model.User;
 import com.seshtutoring.seshapp.util.LayoutUtils;
 import com.seshtutoring.seshapp.util.networking.SeshNetworking;
+import com.seshtutoring.seshapp.view.components.SeshActivityIndicator;
+import com.seshtutoring.seshapp.view.components.SeshAnimatedCheckmark;
 import com.seshtutoring.seshapp.view.components.SeshButton;
+import com.seshtutoring.seshapp.view.components.SeshDialog;
 import com.seshtutoring.seshapp.view.components.SeshEditText;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import me.brendanweinstein.util.ViewUtils;
+
 public class ChangePasswordActivity extends SeshActivity {
 
     private static final String TAG = ChangePasswordActivity.class.getName();
+
 
     private SeshEditText oldPasswordField;
     private SeshEditText newPasswordField;
     private SeshEditText confirmPasswordField;
     private SeshButton changePasswordButton;
     private SeshNetworking seshNetworking;
+    private RelativeLayout requestFlowOverlay;
+    private SeshActivityIndicator activityIndicator;
+    private SeshAnimatedCheckmark animatedCheckmark;
     private User user;
     private ChangePasswordActivity changePasswordActivity;
 
@@ -59,6 +70,10 @@ public class ChangePasswordActivity extends SeshActivity {
         RelativeLayout menuButton = (RelativeLayout) findViewById(R.id.action_bar_menu_button);
         ViewGroup layout = (ViewGroup) menuButton.getParent();
         layout.removeView(menuButton);
+
+        this.requestFlowOverlay = (RelativeLayout) findViewById(R.id.request_flow_overlay);
+        this.activityIndicator = (SeshActivityIndicator) findViewById(R.id.request_activity_indicator);
+        this.animatedCheckmark = (SeshAnimatedCheckmark) findViewById(R.id.animated_check_mark);
 
         RelativeLayout backButton = (RelativeLayout) findViewById(R.id.action_bar_back_button);
         backButton.setOnTouchListener(new View.OnTouchListener() {
@@ -101,25 +116,33 @@ public class ChangePasswordActivity extends SeshActivity {
 
         changePasswordButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                ViewUtils.hideSoftKeyboard(ChangePasswordActivity.this);
                 String oldPassword = oldPasswordField.getText().toString();
                 String newPassword = newPasswordField.getText().toString();
                 String confirmPassword = confirmPasswordField.getText().toString();
-                if (!newPassword.equals(confirmPassword)) {
-                    Toast.makeText(changePasswordActivity, "Confirmation does not match new password.", Toast.LENGTH_LONG).show();
-                }else {
-                    seshNetworking.changePassword(oldPassword, newPassword,
-                            new Response.Listener<JSONObject>() {
-                                @Override
-                                public void onResponse(JSONObject responseJson) {
-                                    onChangePasswordResponse(responseJson);
-                                }
-                            }, new Response.ErrorListener() {
-                                @Override
-                                public void onErrorResponse(VolleyError volleyError) {
-                                    onChangePasswordFailure(volleyError.getMessage());
-                                }
-                            });
+
+                if(newPassword.length() < 5) {
+                    showErrorDialog("Woops!", "Password must be greater than 5 characters");
+                    return;
+                } else if (!newPassword.equals(confirmPassword)) {
+                    showErrorDialog("Woops!", "Password confirmation does not match");
+                    return;
                 }
+
+                requestFlowOverlay.animate().alpha(1).setListener(null).setDuration(300).start();
+
+                seshNetworking.changePassword(oldPassword, newPassword,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject responseJson) {
+                                onChangePasswordResponse(responseJson);
+                            }
+                        }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError volleyError) {
+                                onChangePasswordFailure(volleyError.getMessage());
+                            }
+                        });
             }
         });
     }
@@ -127,23 +150,65 @@ public class ChangePasswordActivity extends SeshActivity {
     private void onChangePasswordResponse(JSONObject responseJson) {
         try {
             if (responseJson.get("status").equals("SUCCESS")) {
-                Toast.makeText(this, "Password updated!", Toast.LENGTH_LONG).show();
-                onBackPressed();
+                hideAnimationWithSuccess(true, "");
+                //onBackPressed();
 //                Intent intent = new Intent(this, MainContainerActivity.class);
 //                startActivity(intent);
             } else if (responseJson.get("status").equals("FAILURE")) {
                 String message = responseJson.get("message").toString();
-                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+                hideAnimationWithSuccess(false, responseJson.getString("message"));
             }
         } catch (JSONException e) {
             Log.e(TAG, e.toString());
-            Toast.makeText(this, "Change Password Failed.", Toast.LENGTH_LONG).show();
+            hideAnimationWithSuccess(false, "Error changing password - please try again later.");
         }
     }
 
     private void onChangePasswordFailure(String errorMessage) {
         Log.e(TAG, "NETWORK ERROR: " + errorMessage);
-        Toast.makeText(this, "We couldn't reach the network, sorry!", Toast.LENGTH_LONG);
+        hideAnimationWithSuccess(false, "Error changing password - please try again later.");
+    }
+
+    private void hideAnimationWithSuccess(final boolean success, final String message) {
+        if (!success) {
+            requestFlowOverlay
+                    .animate()
+                    .setListener(null)
+                    .alpha(0)
+                    .setDuration(300)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            showErrorDialog("Whoops!", message);
+                        }
+                    });
+        } else {
+            activityIndicator
+                    .animate()
+                    .alpha(0)
+                    .setDuration(300)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            super.onAnimationEnd(animation);
+                            animatedCheckmark.setListener(new SeshAnimatedCheckmark.AnimationCompleteListener() {
+                                @Override
+                                public void onAnimationComplete() {
+                                    //setResult(PASSWORD_CHANGED_SUCCESSFULLY_RESPONSE_CODE, null);
+                                    finish();
+                                }
+                            });
+                            animatedCheckmark.startAnimation();
+
+                        }
+                    });
+        }
+    }
+
+    private void showErrorDialog(String title, String message) {
+        SeshDialog.showDialog(getFragmentManager(), title, message,
+                "OKAY", null, "view_request_network_error");
     }
 
     private void hideKeyboard() {
