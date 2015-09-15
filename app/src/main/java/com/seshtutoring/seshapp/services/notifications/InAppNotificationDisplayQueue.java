36057dd7d22816e462632dedd02270670acf85d2
Nadav Hollander <nadavhollander@Nadavs-MacBook-Pro.local>
2015-09-14 17:19:57 -0700

    added periodic refresh notifications

--- app/src/main/java/com/seshtutoring/seshapp/services/notifications/InAppNotificationDisplayQueue.java
+++ app/src/main/java/com/seshtutoring/seshapp/services/notifications/InAppNotificationDisplayQueue.java
@@ -1,6 +1,10 @@
 package com.seshtutoring.seshapp.services.notifications;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 import com.seshtutoring.seshapp.SeshStateManager;
@@ -12,12 +16,16 @@ import com.seshtutoring.seshapp.services.notifications.handlers.NotificationHand
  */
 public class InAppNotificationDisplayQueue {
     private static final String TAG = InAppNotificationDisplayQueue.class.getName();
+    private static final int REFRESH_NOTIFICATIONS_REQUEST_CODE = 21;
+    private static final int FIFTEEN_SECONDS_MILLIS = 15 * 1000;
 
     private static InAppNotificationDisplayQueue mInstance;
 
     public Notification currentNotification;
     private boolean paused;
     private Context mContext;
+    private AlarmManager refreshNotificationsAlarm;
+    private PendingIntent refreshNotificationsPendingIntent;
 
     public static InAppNotificationDisplayQueue sharedInstance(Context context) {
         if (mInstance == null) {
@@ -31,15 +39,23 @@ public class InAppNotificationDisplayQueue {
         this.mContext = context;
         this.paused = true;
         this.currentNotification = null;
+        this.refreshNotificationsAlarm
+                = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(SeshNotificationManagerService.REFRESH_NOTIFICATIONS_ACTION);
+        this.refreshNotificationsPendingIntent = PendingIntent.getService(mContext,
+                REFRESH_NOTIFICATIONS_REQUEST_CODE, intent, PendingIntent.FLAG_UPDATE_CURRENT);
     }
 
     public void resumeHandling() {
         paused = false;
+        refreshNotificationsAlarm.setRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime(), FIFTEEN_SECONDS_MILLIS, refreshNotificationsPendingIntent);
         handleNext();
     }
 
     public void pauseHandling() {
         paused = true;
+        refreshNotificationsAlarm.cancel(refreshNotificationsPendingIntent);
     }
 
     public boolean isPaused() {
