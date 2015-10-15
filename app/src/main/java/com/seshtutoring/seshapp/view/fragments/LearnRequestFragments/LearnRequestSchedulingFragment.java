15754f457762a08552ba7a5a4239412421430093
Nadav Hollander <nadavhollander@DN51ulso.SUNet>
2015-10-14 20:47:25 -0700

    grids and fragments all set up -- need to add blocks

--- app/src/main/java/com/seshtutoring/seshapp/view/fragments/LearnRequestFragments/LearnRequestSchedulingFragment.java
+++ app/src/main/java/com/seshtutoring/seshapp/view/fragments/LearnRequestFragments/LearnRequestSchedulingFragment.java
@@ -1,24 +1,22 @@
 package com.seshtutoring.seshapp.view.fragments.LearnRequestFragments;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
+import android.os.Handler;
+import android.text.Layout;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.seshtutoring.seshapp.R;
 import com.seshtutoring.seshapp.util.DateUtils;
 import com.seshtutoring.seshapp.util.LayoutUtils;
+import com.seshtutoring.seshapp.view.RequestActivity;
 import com.seshtutoring.seshapp.view.components.SeshViewPager;
-import com.seshtutoring.seshapp.view.components.TwoDHScrollView;
-import com.seshtutoring.seshapp.view.components.TwoDVScrollView;
 
 import org.joda.time.DateTime;
 
@@ -30,26 +28,19 @@ import java.util.List;
  */
 public class LearnRequestSchedulingFragment extends SeshViewPager.InputFragment  {
     private static final String TAG = LearnRequestSchedulingFragment.class.getName();
-
-    private SeshViewPager seshViewPager;
-    private VelocityTracker velocityTracker;
-    private TwoDHScrollView hScroll;
-    private TwoDVScrollView vScroll;
-
-    private float mx, my;
-    private float curX, curY;
-
     private boolean isCompleted;
+    private SeshViewPager seshViewPager;
+    private TextView nowButton;
+    private TextView scheduleButton;
+    private RelativeLayout schedulingContainer;
+    private LinearLayout nowScheduleButtons;
+    private TextView whenSeshLabel;
+    private boolean buttonsRaised;
+    private RequestActivity parentActivity;
+    private boolean allowsSwiping;
 
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
         View v = layoutInflater.inflate(R.layout.learn_request_scheduling_fragment, container, false);
-
-//        v.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View view, MotionEvent motionEvent) {
-//                return onTouchEvent(motionEvent);
-//            }
-//        });
 
         LinearLayout columnsContainer = (LinearLayout) v.findViewById(R.id.schedule_columns_container);
         for (int i = 0; i < 7; i++) {
@@ -59,13 +50,119 @@ public class LearnRequestSchedulingFragment extends SeshViewPager.InputFragment 
 
         initDayLabels(v);
 
-        hScroll = (TwoDHScrollView) v.findViewById(R.id.horizontal_scrollview);
-        vScroll = (TwoDVScrollView) v.findViewById(R.id.vertical_scrollview );
+        nowButton = (TextView) v.findViewById(R.id.now_button);
+        scheduleButton = (TextView) v.findViewById(R.id.schedule_button);
+        schedulingContainer = (RelativeLayout) v.findViewById(R.id.scheduling_container);
+        nowScheduleButtons = (LinearLayout) v.findViewById(R.id.now_schedule_buttons);
+        whenSeshLabel = (TextView) v.findViewById(R.id.when_sesh_label);
 
-        velocityTracker = VelocityTracker.obtain();
+        final LayoutUtils utils = new LayoutUtils(getActivity());
 
+        nowButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onNowButtonSelected(utils);
+            }
+        });
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onScheduleButtonSelected(utils);
+            }
+        });
+
+        nowButton.setTypeface(utils.getBookGothamTypeface());
+        scheduleButton.setTypeface(utils.getBookGothamTypeface());
         isCompleted = false;
+        buttonsRaised = false;
+
+        parentActivity = (RequestActivity) getActivity();
         return v;
+    }
+
+    private void onNowButtonSelected(LayoutUtils utils) {
+        setNowButtonSelected(utils);
+
+        if (!buttonsRaised) {
+            animateNowSeshButtonsUp();
+        }
+
+        if (parentActivity.getCurrentLearnRequest().isInstant()) {
+            isCompleted = true;
+            seshViewPager.flingNextFragment();
+        } else {
+            schedulingContainer
+                    .animate()
+                    .x(utils.getScreenWidthPx())
+                    .setDuration(200)
+                    .setStartDelay(0)
+                    .start();
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isCompleted = true;
+                    seshViewPager.flingNextFragment();
+                }
+            }, 300);
+        }
+
+        parentActivity.getCurrentLearnRequest().setIsInstant(true);
+        allowsSwiping = true;
+    }
+
+    private void onScheduleButtonSelected(LayoutUtils utils) {
+        if (!parentActivity.getCurrentLearnRequest().isInstant()) return;
+
+        setScheduleButtonSelected(utils);
+        isCompleted = false;
+        allowsSwiping = false;
+
+        if (!buttonsRaised) {
+            animateNowSeshButtonsUp();
+            schedulingContainer
+                    .animate()
+                    .alpha(1)
+                    .setDuration(300)
+                    .setStartDelay(300)
+                    .setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationStart(Animator animation) {
+                            super.onAnimationStart(animation);
+                            schedulingContainer.setVisibility(View.VISIBLE);
+                        }
+                    })
+                    .start();
+            buttonsRaised = true;
+        } else {
+            schedulingContainer
+                    .animate()
+                    .x(0)
+                    .setDuration(200)
+                    .setStartDelay(0)
+                    .start();
+        }
+
+        parentActivity.getCurrentLearnRequest().setIsInstant(false);
+    }
+
+    private void setScheduleButtonSelected(LayoutUtils utils) {
+        scheduleButton.setTypeface(utils.getMediumGothamTypeface());
+        scheduleButton.setTextColor(getResources().getColor(R.color.seshorange));
+        nowButton.setTypeface(utils.getBookGothamTypeface());
+        nowButton.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    private void setNowButtonSelected(LayoutUtils utils) {
+        nowButton.setTypeface(utils.getMediumGothamTypeface());
+        nowButton.setTextColor(getResources().getColor(R.color.seshorange));
+        scheduleButton.setTypeface(utils.getBookGothamTypeface());
+        scheduleButton.setTextColor(getResources().getColor(R.color.white));
+    }
+
+    private void animateNowSeshButtonsUp() {
+        whenSeshLabel.animate().alpha(0).setDuration(150).start();
+        nowScheduleButtons.animate().y(0).setDuration(300).start();
     }
 
     private void initDayLabels(View v) {
@@ -88,50 +185,6 @@ public class LearnRequestSchedulingFragment extends SeshViewPager.InputFragment 
         }
     }
 
-//    private boolean onTouchEvent(MotionEvent event) {
-//        float curX, curY;
-//        Log.d(TAG, "TOUCH EVENT: " + event);
-//        double flingFactorMs = 0.5;
-//
-//        velocityTracker.addMovement(event);
-//        switch (event.getAction()) {
-//
-//            case MotionEvent.ACTION_DOWN:
-//                mx = event.getX();
-//                my = event.getY();
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                curX = event.getX();
-//                curY = event.getY();
-//                vScroll.scrollBy((int) (mx - curX), (int) (my - curY));
-//                hScroll.scrollBy((int) (mx - curX), (int) (my - curY));
-//                mx = curX;
-//                my = curY;
-//                break;
-//            case MotionEvent.ACTION_UP:
-//                velocityTracker.computeCurrentVelocity(1);
-//                vScroll.smoothScrollBy((int) (velocityTracker.getXVelocity() * flingFactorMs),
-//                        (int) (velocityTracker.getYVelocity() * flingFactorMs));
-//                hScroll.smoothScrollBy((int) (velocityTracker.getXVelocity() * flingFactorMs),
-//                        (int) (velocityTracker.getYVelocity() * flingFactorMs));
-//                break;
-//        }
-//
-//        return true;
-//    }
-//
-//    private class RequestFlowGestureDetector extends GestureDetector.SimpleOnGestureListener {
-//        @Override
-//        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//            if (velocityX < 0) {
-//                flingNextFragment();
-//            } else {
-//                flingPrevFragment();
-//            }
-//            return true;
-//        }
-//    }
-
     @Override
     public void attachSeshViewPager(SeshViewPager seshViewPager) {
         this.seshViewPager = seshViewPager;
@@ -144,6 +197,11 @@ public class LearnRequestSchedulingFragment extends SeshViewPager.InputFragment 
 
     @Override
     public boolean allowsSwiping() {
-        return false;
+        return allowsSwiping;
+    }
+
+    @Override
+    public void onFragmentInForeground() {
+        parentActivity.hideKeyboard();
     }
 }
